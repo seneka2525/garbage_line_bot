6e8112561df0f734ad71081dd37b9986bd049ec0
seneka2525 <stillaki2@gmail.com>
2025-03-24 09:54:15 +0900

    コンフリクト解決

--- garbage_message.py
+++ garbage_message.py
@@ -47,7 +47,7 @@ else:
     message=TextMessage(text=text2)
     line_bot_api.push_message_with_http_info(
         PushMessageRequest(
-            group_id = os.environ.get("a"),
+            group_id = os.environ.get("GROUP_ID"),
             # to = os.environ.get("TO"),
             messages = [message]
         )
